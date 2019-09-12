f63984e08c1e0d7d551d1676a726342991320000
Juliano Lopes <juliano.l@thoughtworks.com>
2019-09-12 15:50:41 -0300

    Adds download and update url

--- src/js/WhatsappWebWithMoreAccessibility.user.js
+++ src/js/WhatsappWebWithMoreAccessibility.user.js
@@ -5,227 +5,227 @@
 // @description  It inserts accessibility on WhatsappWeb
 // @author       Juliano Lopes (https://github.com/juliano-lopes/)
 // @match        *://web.whatsapp.com
-// @downloadURL 
-// @updateURL 
+// @downloadURL https://github.com/juliano-lopes/accessibility-by-force/raw/master/src/js/WhatsappWebWithMoreAccessibility.user.js
+// @updateURL https://github.com/juliano-lopes/accessibility-by-force/raw/master/src/js/WhatsappWebWithMoreAccessibility.user.js
 // @grant        none
 // ==/UserScript==
 
-(function(){
+(function () {
     'use strict';
-var atachInterval;
-var messageInterval;
+    var atachInterval;
+    var messageInterval;
 
-initialButton();
-function initialButton(){
-let initialButton = document.createElement("button");
-initialButton.setAttribute("aria-label", "Ativar script de acessibilidade");
-initialButton.setAttribute("data-status", "off");
-initialButton.addEventListener("click", function(){
-let buttonAccessibility = document.querySelector('[data-status]');
-if(buttonAccessibility.getAttribute("data-status")==="off"){
-if(document.getElementById("pane-side").querySelectorAll('[data-icon="default-user"]').length>0){
-addSearchButton();
-setMainPanelTitle();
-cleanSearchButton();
-messageInterval = setInterval(updateMessage, 1000);
-atachInterval = setInterval(updateAtachLabel, 1500);
-buttonAccessibility.setAttribute("data-status", "on");
-buttonAccessibility.setAttribute("aria-label", "Desativar script de acessibilidade");
-alert('Script de acessibilidade ativado!');
-}
-else{
-alert('Documento ainda sendo carregado...');
-}
-}
-else{
-clearInterval(messageInterval);
-clearInterval(atachInterval);
-removeAccessibilityElements();
-buttonAccessibility.setAttribute("data-status", "off");
-buttonAccessibility.setAttribute("aria-label", "Ativar script de acessibilidade");
-alert("Script de acessibilidade desativado!");
-}
-}, false);
-document.body.insertBefore(initialButton, document.body.firstChild);
-}
+    initialButton();
+    function initialButton() {
+        let initialButton = document.createElement("button");
+        initialButton.setAttribute("aria-label", "Ativar script de acessibilidade");
+        initialButton.setAttribute("data-status", "off");
+        initialButton.addEventListener("click", function () {
+            let buttonAccessibility = document.querySelector('[data-status]');
+            if (buttonAccessibility.getAttribute("data-status") === "off") {
+                if (document.getElementById("pane-side").querySelectorAll('[data-icon="default-user"]').length > 0) {
+                    addSearchButton();
+                    setMainPanelTitle();
+                    cleanSearchButton();
+                    messageInterval = setInterval(updateMessage, 1000);
+                    atachInterval = setInterval(updateAtachLabel, 1500);
+                    buttonAccessibility.setAttribute("data-status", "on");
+                    buttonAccessibility.setAttribute("aria-label", "Desativar script de acessibilidade");
+                    alert('Script de acessibilidade ativado!');
+                }
+                else {
+                    alert('Documento ainda sendo carregado...');
+                }
+            }
+            else {
+                clearInterval(messageInterval);
+                clearInterval(atachInterval);
+                removeAccessibilityElements();
+                buttonAccessibility.setAttribute("data-status", "off");
+                buttonAccessibility.setAttribute("aria-label", "Ativar script de acessibilidade");
+                alert("Script de acessibilidade desativado!");
+            }
+        }, false);
+        document.body.insertBefore(initialButton, document.body.firstChild);
+    }
 
-function updateMessage(){
-setMessageTitle();
-addPlayButtonLabel();
-setConversationTitle();
-}
+    function updateMessage() {
+        setMessageTitle();
+        addPlayButtonLabel();
+        setConversationTitle();
+    }
 
-function updateAtachLabel(){
-addFooterButtonLabels();
-addButtonSendFileLabel();
-addAtachOptionButtonLabel();
-}
+    function updateAtachLabel() {
+        addFooterButtonLabels();
+        addButtonSendFileLabel();
+        addAtachOptionButtonLabel();
+    }
 
-function cleanSearchButton(){
-const backButton = document.querySelector('[data-icon="back-blue"]');
-if(backButton)
-backButton.setAttribute("aria-label", "Limpar e voltar para procura");
-}
+    function cleanSearchButton() {
+        const backButton = document.querySelector('[data-icon="back-blue"]');
+        if (backButton)
+            backButton.setAttribute("aria-label", "Limpar e voltar para procura");
+    }
 
-function setMainPanelTitle(){
-const panel = document.getElementById('pane-side');
-const mainPanelTitle = document.querySelector('[data-main-panel]');
-if(panel){
-if(!mainPanelTitle){
-let h2 = document.createElement("h2");
-let h2Text = document.createTextNode("Painel principal");
-h2.appendChild(h2Text);
-h2.setAttribute("data-sr-only", "pane-side");
-h2.setAttribute("data-main-panel", "pane-side");
-panel.insertBefore(h2, panel.firstChild);
-}
-}
-}
+    function setMainPanelTitle() {
+        const panel = document.getElementById('pane-side');
+        const mainPanelTitle = document.querySelector('[data-main-panel]');
+        if (panel) {
+            if (!mainPanelTitle) {
+                let h2 = document.createElement("h2");
+                let h2Text = document.createTextNode("Painel principal");
+                h2.appendChild(h2Text);
+                h2.setAttribute("data-sr-only", "pane-side");
+                h2.setAttribute("data-main-panel", "pane-side");
+                panel.insertBefore(h2, panel.firstChild);
+            }
+        }
+    }
 
-function addButtonSendFileLabel(){
-let originalSendFile = document.querySelector('[data-icon="send-light"]');
-let cancelSendFile = document.querySelector('[data-icon="x-light"]');
-originalSendFile ? originalSendFile.setAttribute("aria-label", "Enviar arquivo") : "";
-cancelSendFile ? cancelSendFile.setAttribute("aria-label", "Cancelar Envio do arquivo") : "";
-}
+    function addButtonSendFileLabel() {
+        let originalSendFile = document.querySelector('[data-icon="send-light"]');
+        let cancelSendFile = document.querySelector('[data-icon="x-light"]');
+        originalSendFile ? originalSendFile.setAttribute("aria-label", "Enviar arquivo") : "";
+        cancelSendFile ? cancelSendFile.setAttribute("aria-label", "Cancelar Envio do arquivo") : "";
+    }
 
-function removeAccessibilityElements(){
-document.querySelectorAll('[data-sr-only]').forEach(function(accessibilityElement){
-accessibilityElement.parentNode.removeChild(accessibilityElement);
-});
-}
+    function removeAccessibilityElements() {
+        document.querySelectorAll('[data-sr-only]').forEach(function (accessibilityElement) {
+            accessibilityElement.parentNode.removeChild(accessibilityElement);
+        });
+    }
 
-function setConversationTitle(){
-let main = document.getElementById('main');
-if(main){
-let conversation = main.querySelector('[data-sr-only="conversation-title"]');
-let conversationTitle = main.childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("title");
-if(!conversation){
-if(conversationTitle)
-conversationTitle = "Conversa ativa com "+conversationTitle;
-else
-conversationTitle = "Conversa ativa";
-let h3 = document.createElement("h3");
-let h3Text = document.createTextNode(conversationTitle);
-h3.appendChild(h3Text);
-h3.setAttribute("data-sr-only", "conversation-title");
-main.insertBefore(h3, main.firstChild);
-}
-}
-}
+    function setConversationTitle() {
+        let main = document.getElementById('main');
+        if (main) {
+            let conversation = main.querySelector('[data-sr-only="conversation-title"]');
+            let conversationTitle = main.childNodes[1].childNodes[1].childNodes[0].childNodes[0].childNodes[0].getAttribute("title");
+            if (!conversation) {
+                if (conversationTitle)
+                    conversationTitle = "Conversa ativa com " + conversationTitle;
+                else
+                    conversationTitle = "Conversa ativa";
+                let h3 = document.createElement("h3");
+                let h3Text = document.createTextNode(conversationTitle);
+                h3.appendChild(h3Text);
+                h3.setAttribute("data-sr-only", "conversation-title");
+                main.insertBefore(h3, main.firstChild);
+            }
+        }
+    }
 
-function setMessageTitle(){
-let main = document.getElementById('main');
-if(main){
-let msgIn = main.querySelectorAll('div[class*="message-in"]');
-let msgOut = main.querySelectorAll('div[class*="message-out"]');
-if(msgOut.length>0){
-msgOut.forEach(function(message){
-let elSrOnly = message.querySelector('[data-sr-only]');
-if(elSrOnly)
-elSrOnly.parentNode.removeChild(elSrOnly);
-let h4 = document.createElement("h4");
-let h4Text = document.createTextNode("Mensagem enviada:");
-h4.appendChild(h4Text);
-h4.setAttribute("data-sr-only", "msg-out");
-message.insertBefore(h4,message.firstChild);
-});
-}
+    function setMessageTitle() {
+        let main = document.getElementById('main');
+        if (main) {
+            let msgIn = main.querySelectorAll('div[class*="message-in"]');
+            let msgOut = main.querySelectorAll('div[class*="message-out"]');
+            if (msgOut.length > 0) {
+                msgOut.forEach(function (message) {
+                    let elSrOnly = message.querySelector('[data-sr-only]');
+                    if (elSrOnly)
+                        elSrOnly.parentNode.removeChild(elSrOnly);
+                    let h4 = document.createElement("h4");
+                    let h4Text = document.createTextNode("Mensagem enviada:");
+                    h4.appendChild(h4Text);
+                    h4.setAttribute("data-sr-only", "msg-out");
+                    message.insertBefore(h4, message.firstChild);
+                });
+            }
 
-if(msgIn.length>0){
-msgIn.forEach(function(message){
-let elSrOnly = message.querySelector('[data-sr-only]');
-if(elSrOnly)
-elSrOnly.parentNode.removeChild(elSrOnly);
-let h4 = document.createElement("h4");
-let h4Text = document.createTextNode("Mensagem recebida:");
-h4.appendChild(h4Text);
-h4.setAttribute("data-sr-only", "msg-in");
-message.insertBefore(h4,message.firstChild);
-});
-}
-}
-}
+            if (msgIn.length > 0) {
+                msgIn.forEach(function (message) {
+                    let elSrOnly = message.querySelector('[data-sr-only]');
+                    if (elSrOnly)
+                        elSrOnly.parentNode.removeChild(elSrOnly);
+                    let h4 = document.createElement("h4");
+                    let h4Text = document.createTextNode("Mensagem recebida:");
+                    h4.appendChild(h4Text);
+                    h4.setAttribute("data-sr-only", "msg-in");
+                    message.insertBefore(h4, message.firstChild);
+                });
+            }
+        }
+    }
 
-function addPlayButtonLabel(){
-let main = document.getElementById('main');
-if(main){
-let playButtons = main.querySelectorAll('[data-icon="audio-play"]');
-if(playButtons.length>0){
-playButtons.forEach(function(playButton){
-playButton.setAttribute("aria-label", "Play");
-});
-}
-}
-}
+    function addPlayButtonLabel() {
+        let main = document.getElementById('main');
+        if (main) {
+            let playButtons = main.querySelectorAll('[data-icon="audio-play"]');
+            if (playButtons.length > 0) {
+                playButtons.forEach(function (playButton) {
+                    playButton.setAttribute("aria-label", "Play");
+                });
+            }
+        }
+    }
 
-function addAtachOptionButtonLabel(){
-let buttonToAtachFile = document.querySelector('[data-icon="clip"]');
-if(buttonToAtachFile){
-document.querySelector('[data-icon="image"]').setAttribute("aria-label", "Fotos e Vídeos");
-document.querySelector('[data-icon="camera"]').setAttribute("aria-label", "Câmera");
-document.querySelector('[data-icon="document"]').setAttribute("aria-label", "Documento");
-document.querySelector('[data-icon="contact"]').setAttribute("aria-label", "Contato");
-}
-}
+    function addAtachOptionButtonLabel() {
+        let buttonToAtachFile = document.querySelector('[data-icon="clip"]');
+        if (buttonToAtachFile) {
+            document.querySelector('[data-icon="image"]').setAttribute("aria-label", "Fotos e Vï¿½deos");
+            document.querySelector('[data-icon="camera"]').setAttribute("aria-label", "Cï¿½mera");
+            document.querySelector('[data-icon="document"]').setAttribute("aria-label", "Documento");
+            document.querySelector('[data-icon="contact"]').setAttribute("aria-label", "Contato");
+        }
+    }
 
-function addFooterButtonLabels(){
-let footer = document.querySelector('footer');
-if(footer){
-document.querySelector('[data-icon="smiley"]').setAttribute("aria-label", "Smiley");
-document.querySelector('[data-icon="gif"]').setAttribute("aria-label", "GIF");
-document.querySelector('[data-icon="sticker"]').setAttribute("aria-label", "Sticker");
-let buttonToRecord = document.querySelector('[data-icon="ptt"]');
-buttonToRecord ? buttonToRecord.setAttribute("aria-label", "Gravar") : "";
-let buttonToSendText = document.querySelector('[data-icon="send"]');
-buttonToSendText ? buttonToSendText.setAttribute("aria-label", "Enviar") : "";
-let buttonToSendRecordedAudio = document.querySelector('[data-icon="round-send-inv"]');
-buttonToSendRecordedAudio ? buttonToSendRecordedAudio.setAttribute("aria-label", "Enviar") : "";
-let buttonToCancelRecord = document.querySelector('[data-icon="round-x-inv"]');
-buttonToCancelRecord ? buttonToCancelRecord.setAttribute("aria-label", "Cancelar gravação") : "";
-}
-}
+    function addFooterButtonLabels() {
+        let footer = document.querySelector('footer');
+        if (footer) {
+            document.querySelector('[data-icon="smiley"]').setAttribute("aria-label", "Smiley");
+            document.querySelector('[data-icon="gif"]').setAttribute("aria-label", "GIF");
+            document.querySelector('[data-icon="sticker"]').setAttribute("aria-label", "Sticker");
+            let buttonToRecord = document.querySelector('[data-icon="ptt"]');
+            buttonToRecord ? buttonToRecord.setAttribute("aria-label", "Gravar") : "";
+            let buttonToSendText = document.querySelector('[data-icon="send"]');
+            buttonToSendText ? buttonToSendText.setAttribute("aria-label", "Enviar") : "";
+            let buttonToSendRecordedAudio = document.querySelector('[data-icon="round-send-inv"]');
+            buttonToSendRecordedAudio ? buttonToSendRecordedAudio.setAttribute("aria-label", "Enviar") : "";
+            let buttonToCancelRecord = document.querySelector('[data-icon="round-x-inv"]');
+            buttonToCancelRecord ? buttonToCancelRecord.setAttribute("aria-label", "Cancelar gravaï¿½ï¿½o") : "";
+        }
+    }
 
-function addSearchButton(){
-let buttonToSetSearchedTitle = document.createElement("button");
-buttonToSetSearchedTitle.setAttribute("aria-label", "Procurar");
-buttonToSetSearchedTitle.setAttribute("data-sr-only", "search");
-buttonToSetSearchedTitle.addEventListener("click", search, false);
-let mySearch = document.getElementsByTagName("input")[0];
-mySearch.parentNode.insertBefore(buttonToSetSearchedTitle, mySearch.nextSibling);
-}
+    function addSearchButton() {
+        let buttonToSetSearchedTitle = document.createElement("button");
+        buttonToSetSearchedTitle.setAttribute("aria-label", "Procurar");
+        buttonToSetSearchedTitle.setAttribute("data-sr-only", "search");
+        buttonToSetSearchedTitle.addEventListener("click", search, false);
+        let mySearch = document.getElementsByTagName("input")[0];
+        mySearch.parentNode.insertBefore(buttonToSetSearchedTitle, mySearch.nextSibling);
+    }
 
-function search(){
-let mySearch = document.getElementsByTagName("input")[0];
-if(mySearch.value===""){
-alert('A busca está vazia...');
-return;
-}
-setTitleInSearchedResult();
-}
+    function search() {
+        let mySearch = document.getElementsByTagName("input")[0];
+        if (mySearch.value === "") {
+            alert('A busca estï¿½ vazia...');
+            return;
+        }
+        setTitleInSearchedResult();
+    }
 
-function setTitleInSearchedResult() {
-let panel = document.getElementById('pane-side');
-let matchedResults = panel.querySelectorAll('.matched-text');
-if(matchedResults.length>0){
-matchedResults.forEach(function(match){
-let message = match.parentNode;
-let title;
-if(message.getAttribute("title"))
-title = "Conversa de "+message.getAttribute("title")+":";
-else
-title = "Mensagem:";
-if(message.querySelector('[data-sr-only]'))
-message.querySelector('[data-sr-only]').parentNode.removeChild(message.querySelector('[data-sr-only]'));
-let h3 = document.createElement("h3");
-let h3Text = document.createTextNode(title);
-h3.appendChild(h3Text);
-h3.setAttribute("data-sr-only", "title");
-message.insertBefore(h3,match);
-});
-}
-else{
-alert('Nenhum resultado encontrado.');
-}
-}
+    function setTitleInSearchedResult() {
+        let panel = document.getElementById('pane-side');
+        let matchedResults = panel.querySelectorAll('.matched-text');
+        if (matchedResults.length > 0) {
+            matchedResults.forEach(function (match) {
+                let message = match.parentNode;
+                let title;
+                if (message.getAttribute("title"))
+                    title = "Conversa de " + message.getAttribute("title") + ":";
+                else
+                    title = "Mensagem:";
+                if (message.querySelector('[data-sr-only]'))
+                    message.querySelector('[data-sr-only]').parentNode.removeChild(message.querySelector('[data-sr-only]'));
+                let h3 = document.createElement("h3");
+                let h3Text = document.createTextNode(title);
+                h3.appendChild(h3Text);
+                h3.setAttribute("data-sr-only", "title");
+                message.insertBefore(h3, match);
+            });
+        }
+        else {
+            alert('Nenhum resultado encontrado.');
+        }
+    }
 })()
