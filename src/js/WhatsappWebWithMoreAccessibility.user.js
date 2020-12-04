b708552e26a1cc1fd312107f0ff2bc1d12dd37a5
juliano-lopes <julopys@hotmail.com>
2020-12-04 03:06:30 -0300

    Adds description in English and Spanish.

--- src/js/WhatsappWebWithMoreAccessibility.user.js
+++ src/js/WhatsappWebWithMoreAccessibility.user.js
@@ -2,7 +2,7 @@
 // @name         WhatsappWithMoreAccessibility
 // @namespace    https://github.com/juliano-lopes/accessibility-by-force/
 // @version      2.0
-// @description  Este script faz com que o WhatsappWeb se torne mais acessível e tenha uma melhor usabilidade para deficientes visuais usuários de leitores de telas. It puts a better accessibility on WhatsappWeb to screen reader users.
+// @description  Este script faz com que o WhatsappWeb se torne mais acessível e tenha uma melhor usabilidade para deficientes visuais usuários de leitores de telas. It puts a better accessibility on WhatsappWeb to screen reader users. Script para una mejor accesibilidad y usabilidad en WhatsappWeb.
 // @author       Juliano Lopes (https://github.com/juliano-lopes/)
 // @match        https://web.whatsapp.com
 // @downloadURL https://github.com/juliano-lopes/accessibility-by-force/raw/master/src/js/WhatsappWebWithMoreAccessibility.user.js
